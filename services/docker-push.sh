f8bd46a0bb3edda6d8cf9472a5358b59af13621c
Viacheslav Baranov <barash.2018@yandex.ru>
2024-12-10 05:38:27 +0300

    feat: add push to docker registry

---
+++ services/docker-push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker push magenta/highload/cloud-config:dev
+docker push magenta/highload/eureka-server:dev
+docker push magenta/highload/route:dev
+docker push magenta/highload/place:dev
+docker push magenta/highload/api-gateway:dev
+docker push magenta/highload/authentication:dev
+docker push magenta/highload/feedback:dev
+docker push magenta/highload/favorites:dev
+docker push magenta/highload/admin-dashboard:dev
+docker push magenta/highload/files:dev
